dc5bffec35601c8e023263a9cffc966acb5c11fb
Rohit <rrgawanderohit@gmail.com>
2025-04-21 09:18:34 +0530

    basic dependencies and files added

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: "http//localhost:5173",
+  credentails: true,
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at PORT ${PORT}`);
+});
