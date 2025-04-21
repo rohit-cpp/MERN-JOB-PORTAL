253d9dc45fc98529459d889fd043c41cf71b5815
Rohit <rrgawanderohit@gmail.com>
2025-04-22 00:43:52 +0530

    job model controller and route api done

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -19,6 +19,10 @@ const jobSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    experienceLevel: {
+      type: Number,
+      required: true,
+    },
     location: {
       type: String,
       required: true,
