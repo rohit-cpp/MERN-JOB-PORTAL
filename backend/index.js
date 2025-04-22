64933ddd75835297f67ef08cdce0e3bd8d097630
Rohit <rrgawanderohit@gmail.com>
2025-04-22 14:05:38 +0530

    register and login endpoint working successfully

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http//localhost:5173",
-  credentails: true,
+  origin: "http://localhost:5173",
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
